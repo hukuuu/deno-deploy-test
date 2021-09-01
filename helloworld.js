2f2cd54a5ea98f4e2a98333f6f5828a2426909ee
Nikolay Karadzhov <hukuuu@gmail.com>
2021-09-01 20:23:38 +0300

    example deno code

--- helloworld.js
+++ helloworld.js
@@ -1,1 +1,11 @@
-aa
+addEventListener("fetch", (event) => {
+    event.respondWith(
+      new Response("Hello world", {
+        status: 200,
+        headers: {
+          server: "deploy",
+          "content-type": "text/plain",
+        },
+      }),
+    );
+  });
