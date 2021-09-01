bb3b8435caa9d48f4d8dce9e8014416718fedfea
Nikolay Karadzhov <hukuuu@gmail.com>
2021-09-01 20:47:07 +0300

    format

--- helloworld.js
+++ helloworld.js
@@ -3,13 +3,12 @@ addEventListener("fetch", (event) => {
   console.time('Response in')
   event.respondWith(() => {
     console.timeLog('Response in')
-      return new Response("Hello world!", {
-        status: 200,
-        headers: {
-          server: "deploy",
-          "content-type": "text/plain",
-        },
-      }),
-    }
-  );
-});
+    return new Response("Hello world!", {
+      status: 200,
+      headers: {
+        server: "deploy",
+        "content-type": "text/plain",
+      }
+    })
+  })
+})
